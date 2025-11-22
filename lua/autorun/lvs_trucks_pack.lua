02b3552be3a1345f742111f5721c2c7c9d077c62
SpaxscE <stephanklings@freenet.de>
2025-11-22 22:32:23 +0100

    a

---
+++ lua/autorun/lvs_trucks_pack.lua
@@ -0,0 +1,11 @@
+
+if SERVER then
+	--resource.AddWorkshop( "" )
+
+	return
+end
+
+list.Set( "ContentCategoryIcons", "[LVS] - Trucks - Pack", "icon16/lvs_trucks.png" )
+list.Set( "ContentCategoryIcons", "[LVS] - Rig", "icon16/lvs_trucks.png" )
+list.Set( "ContentCategoryIcons", "[LVS] - Rig - Trucks", "icon16/lvs_trucks.png" )
+list.Set( "ContentCategoryIcons", "[LVS] - Rig - Trailers", "icon16/lvs_trucktrailers.png" )
