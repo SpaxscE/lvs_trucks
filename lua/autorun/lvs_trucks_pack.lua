e88daf67f354e7b11012fa34af59dac33f2b35c1
SpaxscE <stephanklings@freenet.de>
2025-11-23 09:54:27 +0100

    move truckbase to base

--- lua/autorun/lvs_trucks_pack.lua
+++ lua/autorun/lvs_trucks_pack.lua
@@ -4,8 +4,3 @@ if SERVER then
 
 	return
 end
-
-list.Set( "ContentCategoryIcons", "[LVS] - Trucks - Pack", "icon16/lvs_trucks.png" )
-list.Set( "ContentCategoryIcons", "[LVS] - Rig", "icon16/lvs_trucks.png" )
-list.Set( "ContentCategoryIcons", "[LVS] - Rig - Trucks", "icon16/lvs_trucks.png" )
-list.Set( "ContentCategoryIcons", "[LVS] - Rig - Trailers", "icon16/lvs_trucktrailers.png" )
